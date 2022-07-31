c17c2d35270b69b2100e0cf44a878730122afdba
luokangwei <110279331+luokangwei@users.noreply.github.com>
2022-07-31 16:42:00 +0800

    hello2.0 commit

--- src/main/java/com/org/GitTest2.java
+++ src/main/java/com/org/GitTest2.java
@@ -3,5 +3,7 @@ package com.org;
 public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello2");
+        
     }
 }
